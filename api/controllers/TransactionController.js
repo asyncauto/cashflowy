6673ef0d0ac30139cbc84ffa4bffbe630bc12eeb
Alex Jv <alex.jv@atherenergy.com>
2018-09-13 19:57:15 +0530

    temporary making transition api available publically

---
+++ api/controllers/TransactionController.js
@@ -0,0 +1,13 @@
+/**
+ * UserController
+ *
+ * this is a blueprint controller
+ * @description :: Server-side logic for managing users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ * 
+ */
+
+module.exports = {
+	
+};
+
