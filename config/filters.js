c83a77089b7087a287e721e9614da1367c11387d
Alex Joseph <alexsanjoseph>
2018-11-17 21:10:03 +0530

    Added yes bank credit card

--- config/filters.js
+++ config/filters.js
@@ -1,18 +1,19 @@
 module.exports.filters = {
 	active: [
-		'IciciCreditCardTransactionAlertFilter',
-		'IciciInternetBankingFilter',
-		'IciciDebitCardFilter', // atm and debit card transactions
-		'IciciCreditCardRefundFilter',
-		'HdfcUpiFilter',
-		'HdfcNetBankingFilter',
-		'CitibankCreditCardTransactionFilter',
-		'AmazonPayTransactionFilter',
-		'AmazonPayCashbackFilter',
-		'AmazonPayExternalFilter',
-		'HdfcBankBalanceFilter', //periodic balance alert from bank
-		'ZerodhaTransferFilter',
-		'HdfcBankAccountCreditFilter'
+		// 'IciciCreditCardTransactionAlertFilter',
+		// 'IciciInternetBankingFilter',
+		// 'IciciDebitCardFilter', // atm and debit card transactions
+		// 'IciciCreditCardRefundFilter',
+		// 'HdfcUpiFilter',
+		// 'HdfcNetBankingFilter',
+		// 'CitibankCreditCardTransactionFilter',
+		// 'AmazonPayTransactionFilter',
+		// 'AmazonPayCashbackFilter',
+		// 'AmazonPayExternalFilter',
+		// 'HdfcBankBalanceFilter', //periodic balance alert from bank
+		// 'ZerodhaTransferFilter',
+		// 'HdfcBankAccountCreditFilter',
+		'YesBankCreditCardTransactionFilter'
 		// 'PaytmFilter',
 	]
 }
