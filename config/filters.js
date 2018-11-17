c296e1467eb5a959080aa8a7cedb27b06af3f246
Alex Joseph <alexsanjoseph>
2018-11-17 21:12:02 +0530

    fixed test comments

--- config/filters.js
+++ config/filters.js
@@ -1,18 +1,18 @@
 module.exports.filters = {
 	active: [
-		// 'IciciCreditCardTransactionAlertFilter',
-		// 'IciciInternetBankingFilter',
-		// 'IciciDebitCardFilter', // atm and debit card transactions
-		// 'IciciCreditCardRefundFilter',
-		// 'HdfcUpiFilter',
-		// 'HdfcNetBankingFilter',
-		// 'CitibankCreditCardTransactionFilter',
-		// 'AmazonPayTransactionFilter',
-		// 'AmazonPayCashbackFilter',
-		// 'AmazonPayExternalFilter',
-		// 'HdfcBankBalanceFilter', //periodic balance alert from bank
-		// 'ZerodhaTransferFilter',
-		// 'HdfcBankAccountCreditFilter',
+		'IciciCreditCardTransactionAlertFilter',
+		'IciciInternetBankingFilter',
+		'IciciDebitCardFilter', // atm and debit card transactions
+		'IciciCreditCardRefundFilter',
+		'HdfcUpiFilter',
+		'HdfcNetBankingFilter',
+		'CitibankCreditCardTransactionFilter',
+		'AmazonPayTransactionFilter',
+		'AmazonPayCashbackFilter',
+		'AmazonPayExternalFilter',
+		'HdfcBankBalanceFilter', //periodic balance alert from bank
+		'ZerodhaTransferFilter',
+		'HdfcBankAccountCreditFilter',
 		'YesBankCreditCardTransactionFilter'
 		// 'PaytmFilter',
 	]
