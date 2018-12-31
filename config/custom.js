3d78d76057f0e8b29bb42d43f91ac1e689330a21
Alex Jv <alexjv89@gmail.com>
2018-12-31 18:05:58 +0530

    bug fix - moving doc filter id to config from environment variables

--- config/custom.js
+++ config/custom.js
@@ -1,7 +1,8 @@
 module.exports = {
     docparser_webhook_secret: process.env.DOCPARSER_WEBHOOK_SECRET,
     docparser_filters: {
-        hdfc_credit_card: process.env.HDFC_CREDIT_CARD_PARSER_ID,
+        hdfc_credit_card:'bzqxicqhpsrk',
+        icici_bank:'sebtifdmvape'
     },
     docparser_api_key: process.env.DOCPARSER_API_KEY
 }
