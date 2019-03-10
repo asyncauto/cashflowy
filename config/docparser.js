c5260d0adccb87db9b3199f41034f48ba4c23db0
Alex Jv <alexjv89@gmail.com>
2019-03-10 18:32:08 +0530

    bug fix

--- config/docparser.js
+++ config/docparser.js
@@ -66,9 +66,9 @@ module.exports.docparser={
 			modifyParsedData:function(parsed_data){
 				if(parsed_data.transactions && parsed_data.transactions.length){
 					if(!parsed_data.transactions_from_date) // if transaction_from_date does not exist
-						parsed_data.transactions_from_date = moment(parsed_data.transactions[0].date, 'MM/DD/YYYY').tz('Asia/Kolkata').toISOString().substring(0,10);
+						parsed_data.transactions_from_date = moment(parsed_data.transactions[0].date, 'YYYY-MM-DD').tz('Asia/Kolkata').toISOString().substring(0,10);
 					if(!parsed_data.transactions_to_date) // if transaction_end_date does not exist
-						parsed_data.transactions_to_date = moment(parsed_data.transactions[parsed_data.transactions.length -1].date, 'MM/DD/YYYY').tz('Asia/Kolkata').toISOString().substring(0,10);
+						parsed_data.transactions_to_date = moment(parsed_data.transactions[parsed_data.transactions.length -1].date, 'YYYY-MM-DD').tz('Asia/Kolkata').toISOString().substring(0,10);
 				}
 				return parsed_data;
 			}
