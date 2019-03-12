b67efd672f637ce10504e6bfb3284525b582685b
akash <akashkupatro@gmail.com>
2019-03-12 17:41:55 +0530

    transaction after create rules.

--- api/models/Transaction.js
+++ api/models/Transaction.js
@@ -4,6 +4,7 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var async = require('async');
 
 module.exports = {
 
@@ -52,10 +53,55 @@ module.exports = {
 	},
 
 	afterCreate: function(created, cb){
-		//if category present return
-		if(created.category) return cb(null);
+		async.auto({
+			getAccount: function (cb) {
+                Account.findOne(created.account).exec(cb);
+            },
+			applyRule: ['getAccount', function(results, cb){
+				Rule.find({user: results.getAccount.user, status: 'active', trigger: 'transaction_after_create'}).exec(function(err,rules){
+					rules.forEach(function(rule){
+						// check if criteria matches the condition
+						var condition = _.get(rule, 'details.trigger.condition',{});
+						
+						// all modification and case specific checks need to be moved to RuleService.
+						if(condition.account)
+							condition.account = parseInt(condition.account)
+						
+						if(condition.amount_inr)
+							if(condition.type == 'income')
+								condition.amount_inr = Math.abs(condition.amount_inr)
+							else
+								condition.amount_inr = -Math.abs(condition.amount_inr)
+
+						condition.type = 'income_expense'
+						if(condition.third_party)
+							if(!created.third_party.includes(condition.third_party))
+								return
+							else
+								delete condition.third_party
+
+						var status = _.isMatch(created, condition);
+						if(status){
+							// executing action here. 
+							if(rule.action=='mark_as_transfer'){
+								_.merge(created,  _.get(rule, 'details.action.set',{}))
+							}
+						}
+					});
+					cb(null, created);
+				})
+			}],
+			updatedTransaction: ['applyRule', function(results, cb){
+				Transaction.update(created.id, created).exec(cb);
+			}],
+			predictCategory: function(cb){
+				//if category present return
+				if(created.category) return cb(null);
+						
+				MLService.predictCategory(created, cb);
+			}
+		}, function(err){cb(err);});
 		
-		MLService.predictCategory(created, cb);
 	},
 
 	afterUpdate: function(updated, cb){
