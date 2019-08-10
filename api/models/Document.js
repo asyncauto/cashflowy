798c1ee9e1fd95494f0e56f8f7ce530c8d6c870f
akash <akashkupatro@gmail.com>
2019-08-11 00:37:01 +0530

    document model created for filestorage

---
+++ api/models/Document.js
@@ -0,0 +1,57 @@
+/**
+ * Document.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    fileName: {
+      type: 'string',
+      columnType: 'text',
+      required: true
+    },
+
+    fd: {
+      type: 'string',
+      columnType: 'text',
+      required: true
+    },
+
+    mime: {
+      type: 'string',
+      columnType: 'text',
+      required: true
+    },
+
+    details: {
+      type: 'json',
+      columnType: 'jsonb'
+    },
+
+    org: {
+      model: 'org',
+      required: true
+    }
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
